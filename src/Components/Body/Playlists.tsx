d45e9ee9e39128b2ee72fbf7589e0362e941e1d7
Heejun Byeon <hejuby@gmail.com>
2022-03-01 04:36:17 +0000

    Studying HTML Drag and Drop api

--- src/Components/Body/Playlists.tsx
+++ src/Components/Body/Playlists.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import './Playlists.css';
 import Playlist from './Playlist';
+
+const onDragOver = (ev: DragEvent) => {
+  ev.preventDefault();
+  console.log("onDragover");
+}
+
+const onDragStart = () => {
+  console.log("onDragStart");
+}
+
+const onDragEnd = () => {
+  console.log("onDragEnd");
+}
+
+const onDrop = () => {
+  console.log("onDrop");
+}
 
 const Playlists = () => 
   <div className="playlists">
     <ul className="lists">
-      {["Custom", "Jazz"].map((name, index) => (
-        <li><Playlist name={ name }/></li>))}
+      {["Custom", "Jazz", "Hiphop", "Classical", "Rock", "Pop"].map((name, index) => (
+        <li
+          key={index}
+          
+          draggable="true"
+          
+          onDragOver={ () => onDragOver }
+          onDragStart={onDragStart}
+          onDragEnd={onDragEnd}
+          onDrop={onDrop}
+        >
+          <Playlist name={ name } />
+        </li>))}
     </ul>
   </div>
 
